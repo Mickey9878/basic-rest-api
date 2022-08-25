5f3eb24c4120894ace1577370e451c2a6749fad2
Mickey9878 <goukaku0035@gmail.com>
2022-08-26 04:23:29 +0800

    add cors for http

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 // const bodyParser = require('body-parser');
 
 const app =express();
 const PORT = 4000;
+
+app.use(cors({origin: true, credentials: true}))
 
 app.use(express.urlencoded({
     extended: false
@@ -10,30 +13,12 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 app.post("/address", (req, res) => {
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     console.log("hello express");
-    res.header('Access-Control-Allow-Origin', '*')
-    const address = req.body.address;
     console.log(req.body);
     return res.json({greet: 'hello'});
     
     //res.sendStatus(500);
 });
-
-app.get("/teststr/:str", async (req, res, next) => {
-    const str = req.params["str"];
-    console.log(`str ${str}`);
-    return res.json(str);
-  }); 
-  
-app.get("/address", (req, res) => {
-    console.log("hello express");
-    res.header('Access-Control-Allow-Origin', '*')
-    const address = req.body.address;
-    console.log(address);
-    res.json({greet: 'hello'});
-    
-    //res.sendStatus(500);
-});
-
 
 app.listen(PORT, () => console.log("running server"));
