de7fb7d9c10d782ce2478dad5b7b965518a66ba8
Mickey9878 <goukaku0035@gmail.com>
2022-08-25 21:34:50 +0800

    new server

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require("express");
+// const bodyParser = require('body-parser');
+
 const app =express();
 const PORT = 4000;
-const userRouter =require("./routes/user")
 
-app.get("/user", (req, res) => {
-    console.log("hello express")
-    res.json({greet: "hello"})
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+app.post("/address", (req, res) => {
+    console.log("hello express");
+    res.header('Access-Control-Allow-Origin', '*')
+    const address = req.body.address;
+    console.log(req.body);
+    return res.json({greet: 'hello'});
+    
     //res.sendStatus(500);
-})
+});
 
-//routing
+app.get("/teststr/:str", async (req, res, next) => {
+    const str = req.params["str"];
+    console.log(`str ${str}`);
+    return res.json(str);
+  }); 
+  
+app.get("/address", (req, res) => {
+    console.log("hello express");
+    res.header('Access-Control-Allow-Origin', '*')
+    const address = req.body.address;
+    console.log(address);
+    res.json({greet: 'hello'});
+    
+    //res.sendStatus(500);
+});
+
 
 app.listen(PORT, () => console.log("running server"));
